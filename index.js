b162a2bde1a7b49f22e0b51d04aca8d228df4288
houssamberrezag <30288998+houssamberrezag@users.noreply.github.com>
2022-11-10 13:25:19 +0100

    ds

--- index.js
+++ index.js
@@ -1,38 +1,7 @@
-import express, { json } from 'express';
-import scraper from '@bochilteam/scraper';
-import {
-    savefrom
-  } from '@bochilteam/scraper';
-
-const app = express();
-
-app.use(json())
-
-const PORT = process.env.PORT || 3000;
-
-app.get('/abc', async (req, res) => {
-const url = req.query.url;
-console.log(url);
-  savefrom(url).then(
-    data=>{
-         res.json( transform(data) )
-    }
-  ).catch(err=>{
-    res.json("not found")
-  });
-   
-});
-
-app.listen(PORT, () => console.log(`App listening at ppport ${PORT}`));
-
-function transform(data){
-    return {
-        img:data.thumb,
-        urls:data.url.map(u=>({
-            url:u.url,
-            quality:u.quality,
-            ext:u.ext,
-            size:u.filesize
-        }))
-    }
-}
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
